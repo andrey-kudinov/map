732e965252e356354b80928bdb5c897ff254ca90
Andrey Kudinov <2499931@gmail.com>
2022-04-30 09:26:25 +0400

    Add logic for table

--- main.js
+++ main.js
@@ -1,10 +1,46 @@
 import './styles/reset.css'
 import './styles/style.scss'
 
+const data = [
+  [1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+  [1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 0, 1, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+  [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+  [1, 0, 1, 0, 0, 1, 0, 1, 0, 0],
+  [1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+]
+
+const createTable = () => {
+  const table = document.querySelector('table')
+
+  data.forEach(d => {
+    const row = d.map(r => {
+      return `
+        <td>
+          &nbsp;
+        <input
+          id=''
+          data-id=${r}
+          name='input'
+          type='checkbox'
+          ${r ? 'checked' : null}
+        />
+          <label></label>
+        </td>
+      `
+    })
+
+    const rowHTML = document.createElement('tr')
+    rowHTML.innerHTML = row.join('')
+    table.append(rowHTML)
+  })
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded')
+  createTable()
 })
-
-window.onload = () => {
-  console.log('window onload')
-}
